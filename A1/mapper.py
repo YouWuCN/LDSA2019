59483200499d60674ae6513a7020eac6bf9d71da
YouWuCN <william.wuy.cn@gmail.com>
2019-04-25 20:15:54 +0000

    Modified mapper.py

--- A1/mapper.py
+++ A1/mapper.py
@@ -1,1 +1,30 @@
+import sys
+
+pronoun = ["han","hon","den","det","denna","denne","hen"]
+
+def read_input(file):
+    for line in file:
+	#split the txt into tweets
+        yield line.split(" ")
+
+def main(separator='\t'):
+    # input comes from STDIN (standard input)
+    data = read_input(sys.stdin)
+    
+    for words in data:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+	pronoun = ["han","hon","den","det","denna","denne","hen"] 
+        for word in words:
+	    word = word.lower()
+	    if word in pronoun:
+		pronoun.remove(word)
+                print('%s%s%d' % (word, separator, 1))
+
+if __name__ == "__main__":
+    main()
+
 
